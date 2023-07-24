0c5d388f800724c233c930d283079e869a1b1c4c
Alana Bookman <bookmana@bu.edu>
2023-07-24 11:55:09 -0400

    code to create a dataframe from a file

---
+++ df.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def load_data(filename, delimiter=' '):
+    df = pd.read_csv(filename, delimiter=delimiter, header=None)
+    df.columns = ['number1', 'number2', 'number3', 'permissions', 'owner', 'group', 'size_in_bytes', 'size_in_kb', 'access_time', 'modification_time', '--', 'full_pathname']
+    return df
+
+df = load_data('filename.list')
