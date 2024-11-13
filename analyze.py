7d2b6fe0917a0b923c7915d91862f0fdb616df24
Brian Gregor <bgregor@bu.edu>
2024-11-13 15:45:23 -0500

    Removed the Dask Bag processing and replaced with just a Dask Dataframe. This is faster and simpler.

--- analyze.py
+++ analyze.py
@@ -1,31 +1,6 @@
 import pandas as pd
 import dask.dataframe as dd
 
-def analyze_data_old(df, levels, gb_threshold, time_threshold):
-    df_in_use = df
-    df_append = pd.DataFrame()
-
-    for l in range(5, levels):
-        pjg = df_in_use.groupby(level=[i for i in range(1,l)]).agg({"size_in_gb": "sum", "access_datetime": "min"})
-
-        # filter out those directories/files that are small or not old enough
-        filtered_df = pjg[(pjg['size_in_gb'] > gb_threshold) | (pjg['access_datetime'] < time_threshold)]
-        filtered_df.to_csv("aggregate_level.csv")
-        level_values = filtered_df.index.get_level_values(l)  
-        filterfurther_df = df[df.index.get_level_values(l - 1).isin(level_values)]
-
-        # select those rows that have file large or old enough but do not have next index level
-        select = filterfurther_df[pd.isna(filterfurther_df.index.get_level_values(l)) & ((filterfurther_df['size_in_gb'] > gb_threshold) | (filterfurther_df['access_datetime'] < time_threshold))]
-        df_append = pd.concat([df_append, select])
-
-        # update the df for next round filter
-        df = filterfurther_df
-
-    df = df[(df['size_in_gb'] > gb_threshold) | (df['access_datetime'] < time_threshold)]
-    final_df = pd.concat([df, df_append])
-    return final_df
-    
-
 def path_extract(full_pathname, levels, level_limit=False):
     ''' For a path like /project/econdept/x/y/z/A.dat
         extract the levels number of dirs
@@ -42,7 +17,6 @@ def analyze_data(df, max_levels, gb_threshold, time_threshold):
     # Create a column for per-level pathnames.
     df['levels_pathname'] = ''
     df_append = []
-
     # Top level of /projectnb/projname is 3.
     for level in range(3, max_levels+1):
         # Create or replace a column in a portion of df to hold the paths for this level.
